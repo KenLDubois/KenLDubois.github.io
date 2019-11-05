633860f3379fb6c27a9f12f7270a087e6470166a
ken dubois <kenldubois@gmail.com>
2019-11-05 08:15:23 -0500

    initial dist folder

---
+++ dist/js/contact.de6139e6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["contact"],{b8fa:function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("h1",[t._v(" Contact Me ")])])}],r=n("2877"),l={},u=Object(r["a"])(l,c,a,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=contact.de6139e6.js.map
---
+++ dist/markdowns/Project0.md
@@ -0,0 +1,13 @@
+this is the amazing details about this project
+
+### Ordered List Example
+
+1. this
+2. is 
+3. a Markdown 
+4. List!
+
+### Unordered List 
+
+- this is 
+- another list!
